e1ae00ccde18f57b34ca5ee2e080db9c56b884d1
unknown <rm.evolveu@gmail.com>
2020-03-24 18:23:20 -0600

    updates to es_LA

--- facility/src/strings/messages_es_LA.js
+++ facility/src/strings/messages_es_LA.js
@@ -2,11 +2,11 @@ const messages_es_LA = {
     languageLabel: "Español",
 
     pojoStrings: {
-        "Hamlet": "Aldea",
-        "Village": "Villa",
-        "Town": "Municipalidad",
-        "Large town": "Ciudad",
-        "City": "Gran Ciudad",
+        "Hamlet": "Ranchería",
+        "Village": "Comunidad",
+        "Town": "Pueblo",
+        "Large town": "Villa",
+        "City": "Ciudad",
 
         "Equator": "Ecuador",
         "Northern Hemisphere": "Hemisferio Norte",
@@ -51,30 +51,30 @@ const messages_es_LA = {
             couldNotLoadCities: "Las ciudades no se pudieron descargar",    
         },
         addCityHandler: {
-            cityHasBeenCreated: "%s1 han sido creadas",
-            weNeedValidCoordinates: "Requerimos de coordinadas validas.",
-            weNeedValidPopulation: "No utilice números negativos ni fraccionarios.",
+            cityHasBeenCreated: "%s1 se ha creado",
+            weNeedValidCoordinates: "Requerimos de coordenadas válidas",
+            weNeedValidPopulation: "No utilice números negativos ni fraccionarios",
             weNeedCityName: "Queremos que nuestras ciudades tengan nombres",    
         },
         moveInHandler: {
-            tryingToEmerge: "Integrando %s1 ciudadanos en %s2...",
-            emerged: "%s1 ciudadanos se han integrado a %s2.",
-            couldNotEmerge: "No se pudo integrar a %s1 ciudadanos en %s2.",
-            noFractions: "No podemos integrar fracciones de ciudadanos.",
-            noNegatives: "Podemos integrar ciudadanos únicamente en numeros positivos."    
+            tryingToEmerge: "Instalando %s1 ciudadanos en %s2...",
+            emerged: "%s1 ciudadanos se han instalado en %s2",
+            couldNotEmerge: "No se pudo instalar a %s1 ciudadanos en %s2",
+            noFractions: "No podemos instalar fracciones de ciudadanos",
+            noNegatives: "Podemos instalar ciudadanos únicamente en numeros positivos"    
         },
         moveOutHandler: {
             tryingToVanish: "Desalojando %s1 ciudadanos de %s2...",
-            vanished: "Se han desalojado %s1 ciudadanos de %s2.",
-            couldNotVanish: "No se pudo deasalojar %s1 ciudadanos de %s2.",
-            noGhostCities: "No nos gustan las ciudades fantasma.",
-            noFractions: "No podemos desalojar fracciones de ciudadanos.",
-            noNegatives: "Podemos desalojar ciudadanos únicamente en numeros positivos."    
+            vanished: "Se han desalojado %s1 ciudadanos de %s2",
+            couldNotVanish: "No se pudo deasalojar a %s1 ciudadanos de %s2",
+            noGhostCities: "No nos gustan las ciudades fantasma",
+            noFractions: "No podemos desalojar fracciones de ciudadanos",
+            noNegatives: "Podemos desalojar ciudadanos únicamente en numeros positivos"    
         },
         pandemizeHandler: {
             tryingToPandemize: "Pandemizando %s1...",
-            pandemized: "%s1 han sido pandemizados.",
-            couldNotPandemize: "No se pudieron pandemizar %s1."    
+            pandemized: "%s1 han sido pandemizad@",
+            couldNotPandemize: "No se pudo pandemizar %s1"    
         }
     }
 }
